8b808547ab8998095b208b040a36c7a6e0e9d86d
Maruha3000 <salonbravia@gmail.com>
2026-07-20 18:53:08 +0100

    Dodaj stronę testową

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-# Koło Fortuny Online
+import streamlit as st
+
+st.set_page_config(page_title="Koło Fortuny Online", page_icon="🎡")
+
+st.title("🎡 Koło Fortuny Online")
+st.write("Strona testowa działa. Niedługo utworzymy pokój i zagramy ze znajomymi.")
